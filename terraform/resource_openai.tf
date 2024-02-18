236037938c522c8c793bb69910a65d5438e3accb
thuneby <thuneby@yahoo.dk>
2024-02-18 15:13:27 +0100

    add OpenAI (#16)
    * add openai
    
    * fix rg
    
    * fix deployment
    
    * Change capacity

---
+++ terraform/resource_openai.tf
@@ -0,0 +1,57 @@
+locals {
+  rg-name         = "openai"
+  deployment-name = "chatmodel"
+  resource-name   = "infrastructure"
+}
+
+resource "azurecaf_name" "rg-openai" {
+  name          = local.rg-name
+  resource_type = "azurerm_resource_group"
+}
+
+resource "azurerm_resource_group" "openai" {
+  name     = azurecaf_name.rg-openai.result
+  location = var.location
+}
+
+resource "azurecaf_name" "cognitive_account" {
+  name          = local.resource-name
+  resource_type = "azurerm_cognitive_account"
+  clean_input   = true
+  # suffixes      = [var.env]
+}
+
+resource "azurerm_cognitive_account" "openai" {
+  name                = azurecaf_name.cognitive_account.result
+  location            = azurerm_resource_group.openai.location
+  resource_group_name = azurerm_resource_group.openai.name
+  kind                = "OpenAI"
+  sku_name            = "S0"
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  depends_on = [
+    azurerm_resource_group.openai
+  ]
+}
+
+resource "azurerm_cognitive_deployment" "chatmodel" {
+  name                 = local.deployment-name
+  cognitive_account_id = azurerm_cognitive_account.openai.id
+
+  model {
+    format = "OpenAI"
+    name   = "gpt-35-turbo"
+  }
+
+  scale {
+    type     = "Standard"
+    capacity = 20
+  }
+
+  depends_on = [
+    azurerm_cognitive_account.openai
+  ]
+}
