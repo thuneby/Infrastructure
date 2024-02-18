87faf98e30530fba60612163036b375a73eaf83a
John Thuneby <thuneby@yahoo.dk>
2024-02-18 14:33:41 +0100

    add openai

---
+++ terraform/resource_openai.tf
@@ -0,0 +1,58 @@
+locals {
+  rg-name         = "openai"
+  deployment-name = "chatmodel"
+  resource-name   = "infrastructure"
+}
+
+resource "azurecaf_name" "rg-openai" {
+  name          = local.rg-name
+  resource_type = "azurerm_resource_group"
+}
+
+resource "azurerm_resource_group" "openai" {
+  name     = azurecaf_name.resource-group.result
+  location = var.location
+}
+
+resource "azurecaf_name" "cognitive_account" {
+  name          = local.resource-name
+  resource_type = "azurerm_cognitive_account"
+  clean_input   = true
+  # suffixes      = [var.env]
+}
+
+resource "azurerm_cognitive_account" "openai" {
+  name                = azurecaf_name.cognitive_account.result
+  location            = azurerm_resource_group.openai.location
+  resource_group_name = azurerm_resource_group.openai.name
+  kind                = "OpenAI"
+  sku_name            = "S0"
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  depends_on = [
+    azurerm_resource_group.openai
+  ]
+}
+
+resource "azurerm_cognitive_deployment" "chatmodel" {
+  name                 = local.deployment-name
+  cognitive_account_id = azurerm_cognitive_account.openai.id
+
+  model {
+    format  = "OpenAI"
+    name    = "gpt-35-turbo"
+    version = "0613"
+  }
+
+  scale {
+    type = "Standard"
+    tier = "Free"
+  }
+
+  depends_on = [
+    azurerm_cognitive_account.openai
+  ]
+}
