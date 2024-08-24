2ded14e3d7c2e14d2356960311c10881b22fc361
John Thuneby <thuneby@yahoo.dk>
2024-08-24 19:23:05 +0200

    update version and remove openai (#44)

--- terraform/resource_openai.tf
+++ terraform/resource_openai.tf
@@ -1,57 +1,57 @@
-locals {
-  rg-name         = "openai"
-  deployment-name = "chatmodel"
-  resource-name   = "infrastructure"
-}
+# locals {
+#   rg-name         = "openai"
+#   deployment-name = "chatmodel"
+#   resource-name   = "infrastructure"
+# }
 
-resource "azurecaf_name" "rg-openai" {
-  name          = local.rg-name
-  resource_type = "azurerm_resource_group"
-}
+# resource "azurecaf_name" "rg-openai" {
+#   name          = local.rg-name
+#   resource_type = "azurerm_resource_group"
+# }
 
-resource "azurerm_resource_group" "openai" {
-  name     = azurecaf_name.rg-openai.result
-  location = var.location
-}
+# resource "azurerm_resource_group" "openai" {
+#   name     = azurecaf_name.rg-openai.result
+#   location = var.location
+# }
 
-resource "azurecaf_name" "cognitive_account" {
-  name          = local.resource-name
-  resource_type = "azurerm_cognitive_account"
-  clean_input   = true
-  # suffixes      = [var.env]
-}
+# resource "azurecaf_name" "cognitive_account" {
+#   name          = local.resource-name
+#   resource_type = "azurerm_cognitive_account"
+#   clean_input   = true
+#   # suffixes      = [var.env]
+# }
 
-resource "azurerm_cognitive_account" "openai" {
-  name                = azurecaf_name.cognitive_account.result
-  location            = azurerm_resource_group.openai.location
-  resource_group_name = azurerm_resource_group.openai.name
-  kind                = "OpenAI"
-  sku_name            = "S0"
+# resource "azurerm_cognitive_account" "openai" {
+#   name                = azurecaf_name.cognitive_account.result
+#   location            = azurerm_resource_group.openai.location
+#   resource_group_name = azurerm_resource_group.openai.name
+#   kind                = "OpenAI"
+#   sku_name            = "S0"
 
-  identity {
-    type = "SystemAssigned"
-  }
+#   identity {
+#     type = "SystemAssigned"
+#   }
 
-  depends_on = [
-    azurerm_resource_group.openai
-  ]
-}
+#   depends_on = [
+#     azurerm_resource_group.openai
+#   ]
+# }
 
-resource "azurerm_cognitive_deployment" "chatmodel" {
-  name                 = local.deployment-name
-  cognitive_account_id = azurerm_cognitive_account.openai.id
+# resource "azurerm_cognitive_deployment" "chatmodel" {
+#   name                 = local.deployment-name
+#   cognitive_account_id = azurerm_cognitive_account.openai.id
 
-  model {
-    format = "OpenAI"
-    name   = "gpt-35-turbo"
-  }
+#   model {
+#     format = "OpenAI"
+#     name   = "gpt-35-turbo"
+#   }
 
-  scale {
-    type     = "Standard"
-    capacity = 20
-  }
+#   scale {
+#     type     = "Standard"
+#     capacity = 20
+#   }
 
-  depends_on = [
-    azurerm_cognitive_account.openai
-  ]
-}
+#   depends_on = [
+#     azurerm_cognitive_account.openai
+#   ]
+# }
