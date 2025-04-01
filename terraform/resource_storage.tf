4194a76b051228eb0b2696c227f2cbd19dd49b6d
John Thuneby <thuneby@yahoo.dk>
2025-04-01 11:46:46 +0200

    fmt

--- terraform/resource_storage.tf
+++ terraform/resource_storage.tf
@@ -34,7 +34,7 @@ resource "azurerm_storage_account" "infrastructure_file_storage" {
 
 resource "azurerm_storage_container" "uploads" {
   name                  = "uploads"
-  storage_account_id  = azurerm_storage_account.infrastructure_file_storage.id
+  storage_account_id    = azurerm_storage_account.infrastructure_file_storage.id
   container_access_type = "private"
 }
 
